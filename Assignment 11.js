d20f9b748cbcfed0227687c4c29cf078c83a5419
RAO FAHAD <raofahad046@gmail.com>
2022-10-06 22:44:19 +0500

    Assignment 11 Completed

---
+++ Assignment 11.js
@@ -0,0 +1,7 @@
+let names = ["Bilal", "Nashit", "Ashar", "Amaan", "Hassan", "Anas"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+console.log(names[5]);
