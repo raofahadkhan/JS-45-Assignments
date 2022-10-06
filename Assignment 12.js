4525fca97f2df97927ab35743dbbeaa8a3de1893
RAO FAHAD <raofahad046@gmail.com>
2022-10-06 22:58:12 +0500

    Assignment 12 Completed

---
+++ Assignment 12.js
@@ -0,0 +1,7 @@
+let names = ["Bilal", "Nashit", "Ashar", "Amaan", "Hassan", "Anas"];
+console.log(`${names[0]} is a Medical Representative`);
+console.log(`${names[1]} is a Web Developer`);
+console.log(`${names[2]} is also a Medical Representative`);
+console.log(`${names[3]} is a Accounts Executive`);
+console.log(`${names[4]} is a Assistant Tax Manager`);
+console.log(`${names[5]} is a Billing Executive`);
