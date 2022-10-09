26942504eff3b775d66f3bdccca757fe26d39ad6
RAO FAHAD <raofahad046@gmail.com>
2022-10-09 23:00:22 +0500

    Assignment 44 Completed

---
+++ Assignment 44.js
@@ -0,0 +1,10 @@
+function makeSandwich(...items) {
+  items.map((item) => {
+    console.log(`Adding ${item} to your Sandwich`);
+  });
+  console.log("Your Sandwich is Ready");
+}
+
+makeSandwich("tomotoes", "ketchup", "chatni");
+makeSandwich("latice", "beef petty", "onions");
+makeSandwich("colelaw", "ketchup", "Green chatni");
