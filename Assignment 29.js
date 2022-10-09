461737238cf86e49f92f923e2ce55fc4ba4befe5
RAO FAHAD <raofahad046@gmail.com>
2022-10-09 19:58:15 +0500

    Assignment 29 Completed

---
+++ Assignment 29.js
@@ -0,0 +1,13 @@
+let favorit_fruits = ["mango", "apple", "banana"];
+
+if (favorit_fruits.includes("mango")) {
+  console.log("I really like mangoes");
+} else if (favorit_fruits.includes("cherry")) {
+  console.log("I really like cherries");
+} else if (favorit_fruits.includes("apple")) {
+  console.log("I really like apples");
+} else if (favorit_fruits.includes("banana")) {
+  console.log("I really like bananas");
+} else {
+  console.log("I dont like fruits");
+}
