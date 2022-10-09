e6930bc40797ca7beba50c580ce96361024c9d37
RAO FAHAD <raofahad046@gmail.com>
2022-10-09 22:43:14 +0500

    Assignment 42 Completed

---
+++ Assignment 42.js
@@ -0,0 +1,23 @@
+let magician_names = ["fahad", "hamza", "sami", "amaan"];
+
+function show_magicians(magician_name) {
+  for (let i = 0; i < magician_name.length; i++) {
+    console.log(magician_name[i]);
+  }
+}
+
+function make_great(magician_name) {
+  let great_magicians = [];
+  magician_name.map((mag_name) => {
+    mag_name1 = mag_name + " The Great";
+    great_magicians.push(mag_name1);
+  });
+  console.log(great_magicians);
+
+  magician_names = great_magicians;
+  return magician_names;
+}
+
+show_magicians(magician_names);
+make_great(magician_names);
+console.log(magician_names);
