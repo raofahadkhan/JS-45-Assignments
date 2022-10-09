97fcc2bbfad0580fe37cec53512892ad8527f374
RAO FAHAD <raofahad046@gmail.com>
2022-10-09 21:41:32 +0500

    Assignment 40 Completed

---
+++ Assignment 40.js
@@ -0,0 +1,20 @@
+function make_album(namee, title, NumOfTracks = 4) {
+  const album = {
+    artist_name: namee,
+    album_title: title,
+    NoOfTracks: NumOfTracks,
+  };
+  return album;
+}
+
+let album = make_album("Atif Aslam", "Taj Dar e Haram");
+console.log(album);
+
+album = make_album("Justin Bieber", "Justice");
+console.log(album);
+
+album = make_album("Rahat Fateh Ali Khan", "Zaroori Tha");
+console.log(album);
+
+album = make_album("Justin Bieber", "Under the Mistletoe", 7);
+console.log(album);
