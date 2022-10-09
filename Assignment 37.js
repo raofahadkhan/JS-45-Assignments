ef6e53d89159b12c00b4318e46f45f96b737202c
RAO FAHAD <raofahad046@gmail.com>
2022-10-09 20:56:55 +0500

    Assignment 37 Completed

---
+++ Assignment 37.js
@@ -0,0 +1,18 @@
+function large_shirt(size = "Large", text = "I Love JavaScript") {
+  console.log(`The size of the T-shirt is ${size}`);
+  console.log(`The Text Printed on T-shirt is ${text}`);
+}
+
+function medium_shirt(size = "Medium", text = "I Love Python") {
+  console.log(`The size of the T-shirt is ${size}`);
+  console.log(`The Text Printed on T-shirt is ${text}`);
+}
+
+function extraLarge_shirt(size = "XXL", text = "Always say the truth") {
+  console.log(`The size of the T-shirt is ${size}`);
+  console.log(`The Text Printed on T-shirt is ${text}`);
+}
+
+large_shirt();
+medium_shirt();
+extraLarge_shirt();
