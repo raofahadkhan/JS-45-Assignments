405549c4e5379ad9714fcc85b0ae572946e6e7c9
RAO FAHAD <raofahad046@gmail.com>
2022-10-09 20:49:40 +0500

    Assignment 36 Completed

---
+++ Assignment 36.js
@@ -0,0 +1,6 @@
+function make_shirt(size, text) {
+  console.log(`The size of the T-shirt is ${size}`);
+  console.log(`The Text Printed on T-shirt is ${text}`);
+}
+
+make_shirt("XXL", "Always say the truth");
