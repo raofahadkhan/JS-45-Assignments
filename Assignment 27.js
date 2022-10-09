043ca79d174a8e51125cbf42ae6ba9d9aeb7e4e5
RAO FAHAD <raofahad046@gmail.com>
2022-10-09 13:33:46 +0500

    Assignment 27 Completed

---
+++ Assignment 27.js
@@ -0,0 +1,43 @@
+let alien_color = "Yellow";
+if (alien_color == "green") {
+  console.log("Player just earned 5 points");
+} else if (alien_color == "Yellow") {
+  console.log("Player just earned 10 points");
+} else if (alien_color == "Red") {
+  console.log("player just earned 15 Points");
+} else {
+  console.log("You lose the game");
+}
+
+alien_color = "green";
+if (alien_color == "green") {
+  console.log("Player just earned 5 points");
+} else if (alien_color == "Yellow") {
+  console.log("Player just earned 10 points");
+} else if (alien_color == "Red") {
+  console.log("player just earned 15 Points");
+} else {
+  console.log("You lose the game");
+}
+
+alien_color = "Red";
+if (alien_color == "green") {
+  console.log("Player just earned 5 points");
+} else if (alien_color == "Yellow") {
+  console.log("Player just earned 10 points");
+} else if (alien_color == "Red") {
+  console.log("player just earned 15 Points");
+} else {
+  console.log("You lose the game");
+}
+
+alien_color = "Magenta";
+if (alien_color == "green") {
+  console.log("Player just earned 5 points");
+} else if (alien_color == "Yellow") {
+  console.log("Player just earned 10 points");
+} else if (alien_color == "Red") {
+  console.log("player just earned 15 Points");
+} else {
+  console.log("You lose the game");
+}
